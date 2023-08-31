15fbfb16409abfd5bedd175f4596b76aeb23d2b7
Pravat Kiran Timsina <ptimsina@horizon.csueastbay.edu>
2023-08-30 18:15:35 -0700

    Anugular Learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld_Angular';
+  message:string = "";
+  display(email:HTMLInputElement,pass:HTMLInputElement){
+    this.message = email.value;
+  }
 }
